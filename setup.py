59831b60e85c4a086ee77d138a477d63b269c509
YangJinJe <yseiren87@gmail.com>
2018-05-28 18:09:39 +0900

    version up to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yangsgoogle',
-    version='0.0.1',
+    version='0.0.2',
     description='Google API Wrapper',
     long_description='Google API Wrapper',
     keywords=['util', 'google'],
